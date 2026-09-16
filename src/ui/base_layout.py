2d5fbc3ad1fb90eeb3d7b64dd323f3eecc7d2e40
Ariesjeev <sahoojeevanbikash@gmail.com>
2026-09-16 22:20:52 +0530

    Update header and subject card styles for improved aesthetics

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -8,11 +8,11 @@ def style_background_home():
         <style>
 
                 .stApp {
-                    background: #5865F2 !important;
+                    background: #1a140d !important;
                 }
 
                 .stApp div[data-testid="stColumn"]{
-                    background-color:#E0E3FF !important;
+                    background-color:#c9a86a !important;
                     padding:2.5rem !important;
                     border-radius: 5rem !important;
                     }
@@ -28,7 +28,7 @@ def style_background_dashboard():
         <style>
 
                 .stApp {
-                    background: #E0E3FF !important;
+                    background: #5d4037 !important;
                 }
 
         </style>  
